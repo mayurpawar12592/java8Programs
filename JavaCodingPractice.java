6ed10c1df08ec3f0ae62df5197d3754fbfc747b2
mayurpawar12592 <mayurpawar12592@gmail.com>
2025-03-13 13:37:52 +0530

    Java 8 Coding

--- JavaCodingPractice.java
+++ JavaCodingPractice.java
@@ -18,7 +18,14 @@ public class JavaCodingPractice {
         List<Integer> distinctList=lst.stream().distinct().collect(Collectors.toList());
         System.out.println("Distinct list "+distinctList);
 
-        //How do you find frequency of each character in a string using Java 8 streams?
+        // program to calculate the sum of integers in a list ?
+        int sum=lst.stream().reduce(0,Integer::sum);
+        System.out.println("sum of element :"+sum);
+
+        int sum1=lst.stream().reduce(0,(no1,no2)->no1+no2);
+        System.out.println("sum of element using reduce :"+sum1);
+
+         //How do you find frequency of each character in a string using Java 8 streams?
         String str="my name is mayur";
         System.out.println("STring is "+str);
         Map<Character,Long> characterCountMap=str.chars().mapToObj(c->(char) c).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
@@ -27,5 +34,13 @@ public class JavaCodingPractice {
         //How do you sort the given list of decimals in reverse order?
         List<Double> decimalList = Arrays.asList(12.45, 23.58, 17.13, 42.89, 33.78, 71.85, 56.98, 21.12);
         decimalList.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+
+        //identify weather given input is number or alphabet
+        char input='5';
+        System.out.println("is isLetterOrDigit :"+Character.isLetterOrDigit(input));
+        System.out.println("is isAlphabetic :"+Character.isAlphabetic(input));
+        System.out.println("is digit :"+Character.isDigit(input));
+
+
     }
 }
