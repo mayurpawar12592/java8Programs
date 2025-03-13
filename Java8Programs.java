feaaeefde1bf69879356049a2f07f85e4127b823
darshw8888 <darsh.wagh18@gmail.com>
2025-03-13 11:03:24 +0530

    Java 8 stream operations

---
+++ Java8Programs.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Java8Programs {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(12, 8, 17, 15, 23, 6);
+        long count = list.stream().count();
+        System.out.println("Count of list no are:-" + count);
+
+        //find ot max no from list
+        int max = list.stream().max(Integer::compareTo).get();
+        System.out.println("Max No is :" + max);
+    }
+}
